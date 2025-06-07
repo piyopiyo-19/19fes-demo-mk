48034ebe0643e10bbe34f734941c57c7160719bb
piyopiyo-19 <kimakasa118@gmail.com>
2025-06-07 16:11:52 +0900

    Add files via upload

--- js/song_forms.js
+++ js/song_forms.js
@@ -83,3 +83,12 @@ function attachFooterDangos() {
   };
   insert();
 }
+
+window.addEventListener('load', () => {
+  const loader = document.getElementById('page-loader');
+  if (loader) {
+    setTimeout(() => {
+      loader.style.display = 'none';
+    }, 300);
+  }
+});
