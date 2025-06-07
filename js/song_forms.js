0d56ca8ea1025d921dccd13105fff64d08b35005
piyopiyo-19 <kimakasa118@gmail.com>
2025-06-07 09:27:39 +0900

    Add files via upload

--- js/song_forms.js
+++ js/song_forms.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
   initHamburgerMenu();
   setupAosAnimations('[data-aos-f]');
   setupPageTopButton(300);
-  attachFooterDangos();
 });
 
 var submitted = false; // used by form iframe callback
@@ -46,22 +45,4 @@ if (document.readyState !== 'loading') {
   createParticles();
 } else {
   document.addEventListener('DOMContentLoaded', createParticles);
-}
-
-function attachFooterDangos() {
-  const dangos = document.querySelector('.footer-dangos');
-  if (!dangos) return;
-
-  const placeholder = document.getElementById('footer-placeholder');
-  if (!placeholder) return;
-
-  const insert = () => {
-    if (placeholder.firstElementChild) {
-      placeholder.style.position = 'relative';
-      placeholder.appendChild(dangos);
-    } else {
-      setTimeout(insert, 100);
-    }
-  };
-  insert();
 }
